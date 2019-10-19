031be2ae7a9b9a68ec6622facecd38af81f0d6a0
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-10-18 20:28:18 -0500

    Initial Commit

---
+++ MLFragments2.0/MLFragments2_0.h
@@ -0,0 +1,19 @@
+//
+//  MLFragments2_0.h
+//  MLFragments2.0
+//
+//  Created by Ricardo Herrera Petit on 10/18/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MLFragments2_0.
+FOUNDATION_EXPORT double MLFragments2_0VersionNumber;
+
+//! Project version string for MLFragments2_0.
+FOUNDATION_EXPORT const unsigned char MLFragments2_0VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MLFragments2_0/PublicHeader.h>
+
+
---
+++ MLFragments2.0Tests/MLFragments2_0Tests.swift
@@ -0,0 +1,34 @@
+//
+//  MLFragments2_0Tests.swift
+//  MLFragments2.0Tests
+//
+//  Created by Ricardo Herrera Petit on 10/18/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import XCTest
+@testable import MLFragments2_0
+
+class MLFragments2_0Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
