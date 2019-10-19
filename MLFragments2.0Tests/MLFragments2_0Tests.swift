96a6d75963844fe6f17697c295c8c599013f4be7
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-10-18 23:29:43 -0500

    Changed If statements to Switch

--- MLFragments2.0Tests/MLFragments2_0Tests.swift
+++ MLFragments2.0Tests/MLFragments2_0Tests.swift
@@ -9,26 +9,62 @@
 import XCTest
 @testable import MLFragments2_0
 
-class MLFragments2_0Tests: XCTestCase {
+enum Emblems : String {
+    case Support
+    case MarksMan
+    case Tank
+    case Jungle
+}
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+class EmblemDater {
+    func getNextAvailableDateFor(emblem:Emblems, currentDate:String) -> String {
+        if currentDate == "2019-10-18T22:00:00-0500" {
+            switch emblem {
+            case .Support:
+                return "2019-10-21T03:00:00-0500"
+            case .MarksMan:
+                return "2019-10-24T03:00:00-0500"
+            case .Tank:
+                return "2019-10-27T03:00:00-0500"
+            default:
+                return "2019-10-30T03:00:00-0500"
+            }
         }
+        return ""
+    }
+}
+
+class MLFragments2_0Tests: XCTestCase {
+    
+    var sut: EmblemDater = EmblemDater()
+    let currentDate = "2019-10-18T22:00:00-0500"
+    
+    func test_NextCurrentAvailableDateForSupportWhenCurrentDateIs18Oct2019() {
+        let nextAvailableDate = sut.getNextAvailableDateFor(emblem: .Support ,currentDate: currentDate)
+        XCTAssertEqual(nextAvailableDate,"2019-10-21T03:00:00-0500")
+        
+    }
+    
+    func test_NextCurrentAvailableDateForMarksManWhenCurrentDateIs18Oct2019() {
+        let nextAvailableDate = sut.getNextAvailableDateFor(emblem: .MarksMan ,currentDate: currentDate)
+        XCTAssertEqual(nextAvailableDate,"2019-10-24T03:00:00-0500")
+        
+    }
+    
+    func test_NextCurrentAvailableDateForTankhenCurrentDateIs18Oct2019() {
+        let nextAvailableDate = sut.getNextAvailableDateFor(emblem: .Tank ,currentDate: currentDate)
+           XCTAssertEqual(nextAvailableDate,"2019-10-27T03:00:00-0500")
+           
+    }
+    
+    func test_NextCurrentAvailableDateForJunglehenCurrentDateIs18Oct2019() {
+        let nextAvailableDate = sut.getNextAvailableDateFor(emblem: .Jungle ,currentDate: currentDate)
+              XCTAssertEqual(nextAvailableDate,"2019-10-30T03:00:00-0500")
+              
     }
 
+    
+    
+    
+    
 }
