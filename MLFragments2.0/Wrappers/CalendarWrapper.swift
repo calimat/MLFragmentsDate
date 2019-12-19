96babe92f80c572b207497eef7487dc31519ccbc
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-12-18 23:06:12 -0500

    Added timezone to calendar

--- MLFragments2.0/Wrappers/CalendarWrapper.swift
+++ MLFragments2.0/Wrappers/CalendarWrapper.swift
@@ -3,12 +3,17 @@ public class CalendarWrapper: CalendarAdapter {
         
     }
     public func isDate(date1:Date, inSameDayAs date2:Date) -> Bool {
-        return Calendar.current.isDate(date1, inSameDayAs: date2)
+        var calendar = Calendar(identifier: .gregorian)
+        let nyTimeZone = TimeZone(identifier: "GMT-5")
+        calendar.timeZone = nyTimeZone!
+        return calendar.isDate(date1, inSameDayAs: date2)
     }
     
     public func date(bySettingHour hour: Int, minute: Int, second: Int, of date: Date) -> Date {
-        
-        return Calendar.current.date(bySettingHour: hour, minute: minute, second: second, of: date)!
+        var calendar = Calendar(identifier: .gregorian)
+        let nyTimeZone = TimeZone(identifier: "GMT-5")
+        calendar.timeZone = nyTimeZone!
+        return calendar.date(bySettingHour: hour, minute: minute, second: second, of: date)!
      }
     
 }
